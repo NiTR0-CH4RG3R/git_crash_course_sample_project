e367feffc3004e064ebd3efa4b05926489670c22
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2023-11-11 17:56:58 +0530

    Used the external_function() defined in the external_function.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <cstdio>
 
+extern void external_function();
+
 int main() {
     printf( "Hello, World!\n" );
     printf( "Did something changing!\n" );
     printf( "Added another line!\n" );
+
+    external_function();
+
     getchar();
     return 0;
 }
