721c1d2bc09cc144d961b6b05c571328f20816f3
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2023-11-11 18:02:02 +0530

    Added another function called external_function_2() to the file

--- src/external_function.cpp
+++ src/external_function.cpp
@@ -4,3 +4,7 @@
 void external_function() {
     printf("Called from the external_function.cpp!\n");
 }
+
+void external_function_2() {
+    printf("Second printf() call from external_function()!\n");
+}
